e7c9f844ff04bee469dd1964b334203c21081945
Joel Turkel <jturkel@salsify.com>
2014-06-12 20:25:02 -0400

    Monkey patch ActiveRecord::Relation instead of ActiveRecord::Base to simplify code and make supporting Rails 3 and 4 easier.

--- lib/goldiloader.rb
+++ lib/goldiloader.rb
@@ -8,31 +8,3 @@ require 'goldiloader/association_helper'
 require 'goldiloader/relation_overrides'
 
 Goldiloader::AssociationOptions.register
-
-class ActiveRecord::Base
-  class << self
-
-    if ::ActiveRecord::VERSION::MAJOR >= 4
-      def all_with_auto_include(*args)
-        result = all_without_auto_include(*args)
-        unless result.is_a?(Goldiloader::RelationOverrides)
-          result = result.clone.extend(Goldiloader::RelationOverrides)
-        end
-        result
-      end
-
-      alias_method_chain :all, :auto_include
-    else
-      def scoped_with_auto_include(*args)
-        result = scoped_without_auto_include(*args)
-        unless result.is_a?(Goldiloader::RelationOverrides)
-          result = result.clone.extend(Goldiloader::RelationOverrides)
-        end
-        result
-      end
-
-      alias_method_chain :scoped, :auto_include
-    end
-
-  end
-end
--- lib/goldiloader/relation_overrides.rb
+++ lib/goldiloader/relation_overrides.rb
@@ -3,18 +3,19 @@
 require 'goldiloader/association_helper'
 require 'goldiloader/model_registry'
 
-module Goldiloader
-  module RelationOverrides
-    def exec_queries
-      return super if loaded?
+ActiveRecord::Relation.class_eval do
 
-      records = super
-      Goldiloader::AssociationHelper.extend_associations(Goldiloader::ModelRegistry.new, records, [])
-      records
-    rescue Exception => e
-      # TODO: Remove
-      STDERR.puts "#{e.message}\n#{e.backtrace.join("\n")}"
-      raise
-    end
+  def exec_queries_with_auto_include
+    return exec_queries_without_auto_include if loaded?
+
+    records = exec_queries_without_auto_include
+    Goldiloader::AssociationHelper.extend_associations(Goldiloader::ModelRegistry.new, records, [])
+    records
+  rescue Exception => e
+    # TODO: Remove
+    STDERR.puts "#{e.message}\n#{e.backtrace.join("\n")}"
+    raise
   end
+
+  alias_method_chain :exec_queries, :auto_include
 end
