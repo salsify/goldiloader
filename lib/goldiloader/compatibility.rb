27ef4853bf1884bc17c94033707d9a2b1eee3b41
Jon Calvert <jonathan.calvert@kitcheck.com>
2015-04-14 10:22:14 -0400

    Gem version changes shouldn't be happening on the fly, so cache these values to avoid lots of unnecessary string comparisons

--- lib/goldiloader/compatibility.rb
+++ lib/goldiloader/compatibility.rb
@@ -4,15 +4,15 @@ module Goldiloader
   module Compatibility
 
     def self.mass_assignment_security_enabled?
-      ::ActiveRecord::VERSION::MAJOR < 4 || defined?(::ActiveRecord::MassAssignmentSecurity)
+      @mass_assignment_security_enabled ||= (::ActiveRecord::VERSION::MAJOR < 4 || defined?(::ActiveRecord::MassAssignmentSecurity))
     end
 
     def self.association_finder_sql_enabled?
-      ::Gem::Version.new(::ActiveRecord::VERSION::STRING) < ::Gem::Version.new('4.1')
+      @association_finder_sql ||= (::Gem::Version.new(::ActiveRecord::VERSION::STRING) < ::Gem::Version.new('4.1'))
     end
 
     def self.unscope_query_method_enabled?
-      ::Gem::Version.new(::ActiveRecord::VERSION::STRING) >= ::Gem::Version.new('4.1')
+      @unscope_query_method ||= ::Gem::Version.new(::ActiveRecord::VERSION::STRING) >= ::Gem::Version.new('4.1')
     end
   end
 end
