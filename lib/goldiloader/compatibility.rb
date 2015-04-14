1ebf793894f9c6962882badf0609792e04cd9609
Jon Calvert <jonathan.calvert@kitcheck.com>
2015-04-14 13:09:05 -0400

    ||= is not appropriate short hand for unless nil if the value can be boolean

--- lib/goldiloader/compatibility.rb
+++ lib/goldiloader/compatibility.rb
@@ -4,15 +4,18 @@ module Goldiloader
   module Compatibility
 
     def self.mass_assignment_security_enabled?
-      @mass_assignment_security_enabled ||= (::ActiveRecord::VERSION::MAJOR < 4 || defined?(::ActiveRecord::MassAssignmentSecurity))
+      @mass_assignment_security_enabled = (::ActiveRecord::VERSION::MAJOR < 4 || defined?(::ActiveRecord::MassAssignmentSecurity)) unless defined?(@mass_assignment_security_enabled)
+      @mass_assignment_security_enabled
     end
 
     def self.association_finder_sql_enabled?
-      @association_finder_sql ||= (::Gem::Version.new(::ActiveRecord::VERSION::STRING) < ::Gem::Version.new('4.1'))
+      @association_finder_sql = (::Gem::Version.new(::ActiveRecord::VERSION::STRING) < ::Gem::Version.new('4.1')) unless defined?(@association_finder_sql)
+      @association_finder_sql
     end
 
     def self.unscope_query_method_enabled?
-      @unscope_query_method ||= ::Gem::Version.new(::ActiveRecord::VERSION::STRING) >= ::Gem::Version.new('4.1')
+      @unscope_query_method = (::Gem::Version.new(::ActiveRecord::VERSION::STRING) >= ::Gem::Version.new('4.1')) unless defined?(@unscope_query_method)
+      @unscope_query_method
     end
   end
 end
