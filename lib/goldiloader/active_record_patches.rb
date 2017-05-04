be49dd0375ebb59d85e23e794f4adececf0c83db
Joel Turkel <Jturkel@salsify.com>
2017-05-04 14:46:22 -0400

    Use Module#prepend instead of alias_method_chain for monkey patching. (#51)

--- lib/goldiloader/active_record_patches.rb
+++ lib/goldiloader/active_record_patches.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 module Goldiloader
-  module AutoIncludableModel
+  module BasePatch
     extend ActiveSupport::Concern
 
     included do
@@ -21,151 +21,139 @@ module Goldiloader
       @auto_include_context = nil
       super
     end
+
+    module ClassMethods
+      # In Rails >= 4.1 has_and_belongs_to_many associations create a has_many associations
+      # under the covers so we need to make sure to propagate the auto_include option to that
+      # association
+      def has_and_belongs_to_many(name, scope = nil, options = {}, &extension)
+        if scope.is_a?(Hash)
+          options = scope
+          scope = nil
+        end
+
+        result = super(name, scope, options, &extension)
+        if options.include?(:auto_include)
+          _reflect_on_association(name).options[:auto_include] = options[:auto_include]
+        end
+        result
+      end
+    end
   end
-end
+  ::ActiveRecord::Base.include(::Goldiloader::BasePatch)
 
-ActiveRecord::Base.send(:include, Goldiloader::AutoIncludableModel)
+  module RelationPatch
+    def exec_queries
+      return super if loaded?
 
-ActiveRecord::Relation.class_eval do
-
-  def exec_queries_with_auto_include(&block)
-    return exec_queries_without_auto_include(&block) if loaded?
-
-    models = exec_queries_without_auto_include(&block)
-    Goldiloader::AutoIncludeContext.register_models(models, eager_load_values)
-    models
+      models = super
+      Goldiloader::AutoIncludeContext.register_models(models, eager_load_values)
+      models
+    end
   end
+  ::ActiveRecord::Relation.prepend(::Goldiloader::RelationPatch)
 
-  Goldiloader::Compatibility.alias_method_chain self, :exec_queries, :auto_include
-end
+  module AssociationPatch
+    extend ActiveSupport::Concern
 
-ActiveRecord::Associations::Association.class_eval do
+    included do
+      class_attribute :default_auto_include, :default_fully_load
+      self.default_auto_include = true
+      self.default_fully_load = false
+    end
 
-  class_attribute :default_auto_include, :default_fully_load
-  self.default_auto_include = true
-  self.default_fully_load = false
+    def auto_include?
+      # We only auto include associations that don't have in-memory changes since the
+      # Rails association Preloader clobbers any in-memory changes
+      !loaded? && target.blank? && options.fetch(:auto_include) { self.class.default_auto_include } && eager_loadable?
+    end
 
-  def auto_include?
-    # We only auto include associations that don't have in-memory changes since the
-    # Rails association Preloader clobbers any in-memory changes
-    !loaded? && target.blank? && options.fetch(:auto_include) { self.class.default_auto_include } && eager_loadable?
-  end
+    def fully_load?
+      !loaded? && options.fetch(:fully_load) { self.class.default_fully_load }
+    end
 
-  def fully_load?
-    !loaded? && options.fetch(:fully_load) { self.class.default_fully_load }
-  end
+    private
 
-  private
-
-  def eager_loadable?
-    association_info = Goldiloader::AssociationInfo.new(self)
-    !association_info.limit? &&
+    def eager_loadable?
+      association_info = Goldiloader::AssociationInfo.new(self)
+      !association_info.limit? &&
         !association_info.offset? &&
         !association_info.group? &&
         !association_info.from? &&
         !association_info.instance_dependent?
-  end
-
-  def load_with_auto_include(load_method, *args)
-    if loaded? && !stale_target?
-      target
-    elsif auto_include?
-      Goldiloader::AssociationLoader.load(owner, reflection.name)
-      target
-    else
-      send("#{load_method}_without_auto_include", *args)
-    end
-  end
-
-end
-
-ActiveRecord::Associations::SingularAssociation.class_eval do
-
-  private
-
-  def find_target_with_auto_include(*args)
-    load_with_auto_include(:find_target, *args)
-  end
-
-  Goldiloader::Compatibility.alias_method_chain self, :find_target, :auto_include
-end
-
-ActiveRecord::Associations::CollectionAssociation.class_eval do
-  # Force these methods to load the entire association for fully_load associations
-  association_methods = [:size, :ids_reader, :empty?]
-  if Goldiloader::Compatibility::ACTIVE_RECORD_VERSION < ::Gem::Version.new('5.1')
-    association_methods.concat([:first, :second, :third, :fourth, :fifth, :last])
-  end
-
-  association_methods.each do |method|
-    aliased_target, punctuation = method.to_s.sub(/([?!=])$/, ''), $1
-    define_method("#{aliased_target}_with_fully_load#{punctuation}") do |*args, &block|
-      load_target if fully_load?
-      send("#{aliased_target}_without_fully_load#{punctuation}", *args, &block)
     end
 
-    Goldiloader::Compatibility.alias_method_chain self, method, :fully_load
-  end
-
-  private
-
-  def load_target_with_auto_include(*args)
-    load_with_auto_include(:load_target, *args)
-  end
-
-  Goldiloader::Compatibility.alias_method_chain self, :load_target, :auto_include
-
-  if Goldiloader::Compatibility::ACTIVE_RECORD_VERSION >= ::Gem::Version.new('5.1')
-    def find_from_target_with_fully_load?
-      fully_load? || find_from_target_without_fully_load?
+    def load_with_auto_include
+      if loaded? && !stale_target?
+        target
+      elsif auto_include?
+        Goldiloader::AssociationLoader.load(owner, reflection.name)
+        target
+      else
+        yield
+      end
     end
-    Goldiloader::Compatibility.alias_method_chain self, :find_from_target?, :fully_load
   end
+  ::ActiveRecord::Associations::Association.include(::Goldiloader::AssociationPatch)
 
-end
+  module SingularAssociationPatch
+    private
 
-[ActiveRecord::Associations::HasManyThroughAssociation, ActiveRecord::Associations::HasOneThroughAssociation].each do |klass|
-  klass.class_eval do
+    def find_target(*args)
+      load_with_auto_include { super }
+    end
+  end
+  ::ActiveRecord::Associations::SingularAssociation.prepend(::Goldiloader::SingularAssociationPatch)
+
+  module CollectionAssociationPatch
+    # Force these methods to load the entire association for fully_load associations
+    association_methods = [:size, :ids_reader, :empty?]
+    if Goldiloader::Compatibility::ACTIVE_RECORD_VERSION < ::Gem::Version.new('5.1')
+      association_methods.concat([:first, :second, :third, :fourth, :fifth, :last])
+    end
+
+    association_methods.each do |method|
+      define_method(method) do |*args, &block|
+        load_target if fully_load?
+        super(*args, &block)
+      end
+    end
+
+    def load_target(*args)
+      load_with_auto_include { super }
+    end
+
+    if Goldiloader::Compatibility::ACTIVE_RECORD_VERSION >= ::Gem::Version.new('5.1')
+      def find_from_target?
+        fully_load? || super
+      end
+    end
+  end
+  ::ActiveRecord::Associations::CollectionAssociation.prepend(::Goldiloader::CollectionAssociationPatch)
+
+  module ThroughAssociationPatch
     def auto_include?
       # Only auto include through associations if the target association is auto-loadable
       through_association = owner.association(through_reflection.name)
       through_association.auto_include? && super
     end
   end
-end
+  ::ActiveRecord::Associations::HasManyThroughAssociation.prepend(::Goldiloader::ThroughAssociationPatch)
+  ::ActiveRecord::Associations::HasOneThroughAssociation.prepend(::Goldiloader::ThroughAssociationPatch)
 
-# In Rails >= 4.1 has_and_belongs_to_many associations create a has_many associations
-# under the covers so we need to make sure to propagate the auto_include option to that
-# association
-ActiveRecord::Associations::ClassMethods.class_eval do
-
-  def has_and_belongs_to_many_with_auto_include_option(name, scope = nil, options = {}, &extension)
-    if scope.is_a?(Hash)
-      options = scope
-      scope = nil
-    end
-
-    result = has_and_belongs_to_many_without_auto_include_option(name, scope, options, &extension)
-    if options.include?(:auto_include)
-      _reflect_on_association(name).options[:auto_include] = options[:auto_include]
-    end
-    result
-  end
-
-  Goldiloader::Compatibility.alias_method_chain self, :has_and_belongs_to_many, :auto_include_option
-end
-
-# The CollectionProxy just forwards exists? to the underlying scope so we need to intercept this and
-# force it to use size which handles fully_load properly.
-ActiveRecord::Associations::CollectionProxy.class_eval do
-  def exists?(*args)
-    # We don't fully_load the association when arguments are passed to exists? since Rails always
-    # pushes this query into the database without any caching (and it likely not a common
-    # scenario worth optimizing).
-    if args.empty? && @association.fully_load?
-      size > 0
-    else
-      scope.exists?(*args)
+  module CollectionProxyPatch
+    # The CollectionProxy just forwards exists? to the underlying scope so we need to intercept this and
+    # force it to use size which handles fully_load properly.
+    def exists?(*args)
+      # We don't fully_load the association when arguments are passed to exists? since Rails always
+      # pushes this query into the database without any caching (and it likely not a common
+      # scenario worth optimizing).
+      if args.empty? && @association.fully_load?
+        size > 0
+      else
+        scope.exists?(*args)
+      end
     end
   end
+  ::ActiveRecord::Associations::CollectionProxy.prepend(::Goldiloader::CollectionProxyPatch)
 end
--- lib/goldiloader/compatibility.rb
+++ lib/goldiloader/compatibility.rb
@@ -3,29 +3,5 @@
 module Goldiloader
   module Compatibility
     ACTIVE_RECORD_VERSION = ::Gem::Version.new(::ActiveRecord::VERSION::STRING)
-
-    # Copied from Rails since it is deprecated in Rails 5.0. Switch to using
-    # Module#prepend when we drop Ruby 1.9 support.
-    def self.alias_method_chain(klass, target, feature)
-      # Strip out punctuation on predicates, bang or writer methods since
-      # e.g. target?_without_feature is not a valid method name.
-      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-      yield(aliased_target, punctuation) if block_given?
-
-      with_method = "#{aliased_target}_with_#{feature}#{punctuation}"
-      without_method = "#{aliased_target}_without_#{feature}#{punctuation}"
-
-      klass.send(:alias_method, without_method, target)
-      klass.send(:alias_method, target, with_method)
-
-      case
-      when klass.public_method_defined?(without_method)
-        klass.send(:public, target)
-      when klass.protected_method_defined?(without_method)
-        klass.send(:protected, target)
-      when klass.private_method_defined?(without_method)
-        klass.send(:private, target)
-      end
-    end
   end
 end
