bde7dd3ca58c67a5fe16e1ba9f72666e3bfb7b0a
Joel Turkel <Jturkel@salsify.com>
2019-10-03 12:34:06 -0400

    Remove dead code (#91)

--- lib/goldiloader/association_loader.rb
+++ lib/goldiloader/association_loader.rb
@@ -18,16 +18,6 @@ module Goldiloader
       ::ActiveRecord::Associations::Preloader.new.preload(models, [association_name])
     end
 
-    def first_model_with_association(models, association_name)
-      models.find { |model| has_association?(model, association_name) }
-    end
-
-    def associated_models(models, association_name)
-      # We can't just do model.send(association_name) because the association method may have been
-      # overridden
-      models.map { |model| model.association(association_name).target }.flatten.compact.uniq
-    end
-
     def load?(model, association_name)
       # Need to make sure the model actually has the association which won't always
       # be the case in STI hierarchies e.g. only a subclass might have the association
--- lib/goldiloader/compatibility.rb
+++ lib/goldiloader/compatibility.rb
@@ -4,19 +4,12 @@ module Goldiloader
   module Compatibility
     ACTIVE_RECORD_VERSION = ::Gem::Version.new(::ActiveRecord::VERSION::STRING)
     RAILS_5_2_0 = ACTIVE_RECORD_VERSION == ::Gem::Version.new('5.2.0')
-    PRE_RAILS_5_2 = ACTIVE_RECORD_VERSION < ::Gem::Version.new('5.2.0')
-    POST_RAILS_5_1_4 = ACTIVE_RECORD_VERSION > ::Gem::Version.new('5.1.5')
-    PRE_RAILS_5_1_5 = ACTIVE_RECORD_VERSION < ::Gem::Version.new('5.1.5')
     FROM_EAGER_LOADABLE = ACTIVE_RECORD_VERSION >= ::Gem::Version.new('5.1.5') ||
       (ACTIVE_RECORD_VERSION >= ::Gem::Version.new('5.0.7') && ACTIVE_RECORD_VERSION < ::Gem::Version.new('5.1.0'))
     GROUP_EAGER_LOADABLE = FROM_EAGER_LOADABLE
 
     def self.rails_4?
       ::ActiveRecord::VERSION::MAJOR == 4
-    end
-
-    def self.rails_5_0?
-      ::ActiveRecord::VERSION::MAJOR == 5 && ::ActiveRecord::VERSION::MINOR == 0
     end
 
     # See https://github.com/rails/rails/pull/32375
