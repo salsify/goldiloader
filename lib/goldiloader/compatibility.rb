62af77b5bc6e6820d8944d9859aafc784da316fe
Joel Turkel <Jturkel@salsify.com>
2024-07-30 16:14:44 -0400

    Use assert_modifiable! rather than assert_mutability! in Rails 7.2+. (#151)

--- lib/goldiloader/compatibility.rb
+++ lib/goldiloader/compatibility.rb
@@ -13,5 +13,10 @@ module Goldiloader
     def self.rails_6_1_or_greater?
       ::ActiveRecord::VERSION::MAJOR > 6 || rails_6_1?
     end
+
+    def self.pre_rails_7_2?
+      ::ActiveRecord::VERSION::MAJOR < 7 ||
+        (::ActiveRecord::VERSION::MAJOR == 7 && ::ActiveRecord::VERSION::MINOR < 2)
+    end
   end
 end
